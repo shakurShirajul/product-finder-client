7ab4df82d7fabe69d8520ac8a96d65c7645063c2
shakurShirajul <siirajulisllam@gmail.com>
2024-08-15 20:49:19 +0600

    Login Register Page Added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/Login/Login";
 import Root from "../Layout/Root";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root />,
     children: [
       {
         path: "/login",
         element: <Login />,
       },
+      {
+        path: "/login",
+        element: <Register/>
+      }
     ],
   },
 ]);
